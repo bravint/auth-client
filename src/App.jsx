3a81484e14ce214e2d7505c725a29405da511576
bravint <bravint@gmail.com>
2022-02-14 15:50:37 +0000

    requirement 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,12 @@ export default function App() {
 
     const [form, setForm] = useState(formInitialState);
     const [currentUser, setCurrentUser] = useState(null);
+    const [token, setToken] = useState(null);
 
-    console.log(form);
+    console.log(`states`, {
+        form,
+        currentUser,
+    });
 
     const fetchConfig = (reqBody) => {
         return {
@@ -23,16 +27,31 @@ export default function App() {
         };
     };
 
-    const postLogin = async (form) => {
-        const response = await fetch(`http://localhost:4000/register`, fetchConfig(form));
-        const data = await response.json();
+    const postLogin = async (form, endpoint) => {
+        try{
+            const response = await fetch(`http://localhost:4000/${endpoint}`, fetchConfig(form));
+
+            return await response.json();
+        } catch(error) {
+            console.log(error)
+        }
+
+    };
+
+    const handleSubmitRegister = async (event) => {
+        event.preventDefault();
+
+        const data = await postLogin(form, 'register');
+
         setCurrentUser(data);
     };
 
-    const handleSubmit = async (event) => {
+    const handleSubmitLogin = async (event) => {
         event.preventDefault();
 
-        await postLogin(form);
+        const data = await postLogin(form, 'login');
+
+        setToken(data);   
     };
 
     const handleChange = (event) => {
@@ -42,18 +61,46 @@ export default function App() {
         setForm({ ...form, [name]: value });
     };
 
+    const handleClickLogOut = () => {
+        setCurrentUser(null);
+    };
+
     return (
         <div className="App">
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="username" placeholder="User Name" onChange={handleChange} />
-                <input type="password" name="password" placeholder="Password" onChange={handleChange} />
-                <button type="submit">Login</button>
-            </form>
+            {!currentUser && (
+                <>
+                    <h1>Register</h1>
+                    <form id="register" onSubmit={handleSubmitRegister}>
+                        <input type="text" name="username" placeholder="User Name" onChange={handleChange} />
+                        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+                        <button type="submit">Login</button>
+                    </form>
+                    <br></br>
+                    <h1>Login</h1>
+                    <form id="login" onSubmit={handleSubmitLogin}>
+                        <input type="text" name="username" placeholder="User Name" onChange={handleChange} />
+                        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+                        <button type="submit">Login</button>
+                    </form>
+                </>
+            )}
             {currentUser && (
-                <div>
-                    <h3>Current User</h3>
-                    <p>{currentUser.username}</p>
-                </div>
+                <>
+                    <div>
+                        <h3>Current User</h3>
+                        <p>{currentUser.username}</p>
+                    </div>
+                    <button onClick={handleClickLogOut}>Log-out</button>
+                </>
+            )}
+            {token && (
+                <>
+                    <div>
+                        <h3>JWT</h3>
+                        <p>{token}</p>
+                    </div>
+                    <button onClick={handleClickLogOut}>Log-out</button>
+                </>
             )}
         </div>
     );
